f2b122f36b39ce34bcf27bece3f01a4a3f72d518
Nick Thomas <nick.thomas@invintus.com>
2016-09-10 23:15:24 -0700

    emit error only once

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function combineLatest (streams) {
     }
     var haveData = false
     var err = false
+    var allDone = false
 
     var abort = null
     var emit
@@ -21,7 +22,9 @@ function combineLatest (streams) {
 
     streams.forEach(function sink (s, i) {
         process.nextTick(function () {
+            if (allDone) return
             s(abort, function onData (end, data) {
+                if (err) return
                 if (end === true) {
                     ended[i] = true
                     var allEnded = ended.every(function (e) {
@@ -32,8 +35,10 @@ function combineLatest (streams) {
                     }
                     return
                 }
+
                 err = end || err
                 if (err) {
+                    allDone = true
                     return emit(err)
                 }
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var S = require('pull-stream')
+var Pushable = require('pull-pushable')
 var combine = require('../')
 
 test('combine latest', function (t) {
@@ -16,9 +17,35 @@ test('combine latest', function (t) {
     S(
         combine(S.values([1,2,3]), S.values(['a','b','c'])),
         S.collect(function (err, data) {
-            console.log('in here', data)
             t.error(err, 'should not have error')
             t.deepEqual(data, expected, 'should emit the right data')
         })
     )
+})
+
+test('error handling', function (t) {
+    t.plan(2)
+
+    var p1 = Pushable()
+    var p2 = Pushable()
+
+    function sink (source) {
+        source(null, function onEvent (end, data) {
+            if (end === true) return t.fail('should not end')
+            if (end) return t.equal(end.message, 'test error',
+                'should emit error once')
+            t.deepEqual(data, ['data', 'more data'], 'should emit data once')
+            source(null, onEvent)
+        })
+    }
+
+    S(
+        combine(p1, p2),
+        sink
+    )
+
+    p1.push('data')
+    p2.push('more data')
+    p1.end(new Error('test error'))
+    p2.push('data 3')
 })
